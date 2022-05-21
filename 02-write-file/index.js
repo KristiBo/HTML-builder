1dbecc445c18776e43d3750eec882f62948fd15f
KristiBo <kristina.boroh@gmail.com>
2022-05-22 02:43:42 +0300

    feat: add solutions to 01 and 02 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'file.txt');
+const { stdin, stdout, exit } = process;
+const readline = require('readline');
+const input = readline.createInterface(stdin);
+const output = fs.createWriteStream(pathFile);
+
+stdout.write('Добрый день! Введите, пожалуйста, текст\n');
+
+const end = () => {
+  stdout.write('Всего доброго!');
+  exit();
+}
+
+input.on('line', (text) => {
+  if (text === 'exit') end();
+  output.write(text + '\n');
+});
+
+process.on('SIGINT', end);
