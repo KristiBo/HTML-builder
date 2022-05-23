321c8c236191efe6bb8bc7538978c0114962e19a
KristiBo <kristina.boroh@gmail.com>
2022-05-23 13:44:53 +0300

    feat: add solution to 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const pathFolder = path.join(__dirname, 'styles');
+const bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+
+fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
+  if (error) console.error('Error');
+  const writeStream = fs.createWriteStream(bundleFile);
+  for (let file of files) {
+    if (file.isFile() && path.parse(file.name).ext === '.css') {
+      const pathFile = path.join(pathFolder, file.name);
+      const readStream = fs.createReadStream(pathFile, 'utf-8');
+      readStream.pipe(writeStream, {end: false});
+      readStream.on('end', function () {
+        writeStream.write('');
+      });
+    }
+  }
+});
